65a1e52c17548c0a18530c8eacaba4c19fc7f77f
Sawom <asawom250@gmail.com>
2025-02-21 10:09:30 +0600

    about page

---
+++ src/app/about/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <p className='font-bold text-3xl'>this is about page</p>
+        </div>
+    );
+};
+
+export default AboutPage;
