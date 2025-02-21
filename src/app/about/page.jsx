911d6f8925dd4d68d139beb20e79051d62701a8a
Sawom <asawom250@gmail.com>
2025-02-21 11:19:43 +0600

    navigation navbar added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,24 @@
-import Link from "next/link";
+"use client";
+import { useRouter } from "next/navigation";
 
 const AboutPage = () => {
+  const router = useRouter();
+
+  const isLoggedIn = true;
+  const handleNavigation = () => {
+    if (isLoggedIn) {
+      router.push("/about/address");
+    } else {
+      router.push("/");
+    }
+  };
+
   return (
     <div>
       <p className="font-bold text-3xl">this is about page</p>
-      <Link href="/about/address"></Link>
+      <button type="button" className="mt-4" onClick={handleNavigation}>
+        address page
+      </button>
     </div>
   );
 };
