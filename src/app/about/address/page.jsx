eb35dca48e90e416485a3587cf6ac5445cde68ba
Sawom <asawom250@gmail.com>
2025-02-21 10:13:34 +0600

    address pages

---
+++ src/app/about/address/page.jsx
@@ -0,0 +1,10 @@
+
+const AddressPage = () => {
+    return (
+        <div>
+            <p className='font-bold text-3xl'>this is address page under about</p>
+        </div>
+    );
+};
+
+export default AddressPage;
