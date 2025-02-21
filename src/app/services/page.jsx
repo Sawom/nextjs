76b81d455ad4bf55b87e59462aa206271e24ffe9
Sawom <asawom250@gmail.com>
2025-02-21 10:07:20 +0600

    service pages

---
+++ src/app/services/page.jsx
@@ -0,0 +1,9 @@
+const ServicesPage = () => {
+    return (
+        <div>
+           <p className="font-bold text-3xl">this is services</p> 
+        </div>
+    );
+};
+
+export default ServicesPage;
