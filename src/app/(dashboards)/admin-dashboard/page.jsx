cf50b4d29b02f11af6a771594db273da5bca0dbc
Sawom <asawom250@gmail.com>
2025-02-22 08:26:30 +0600

    dashboard setup

---
+++ src/app/(dashboards)/admin-dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdminDashboard from './page';
+
+const AdminDashboard = () => {
+    return (
+        <div>
+            <p className='font-bold text-3xl'>this is Admin Dashboard</p>
+        </div>
+    );
+};
+
+export default AdminDashboard;
