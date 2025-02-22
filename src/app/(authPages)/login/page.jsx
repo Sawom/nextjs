cc314194f27bbb9b4bd64507c31751c8e969b27c
Sawom <asawom250@gmail.com>
2025-02-22 08:16:15 +0600

    auth pages

---
+++ src/app/(authPages)/login/page.jsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <div>
+      <p className="font-bold text-3xl">this is login page</p>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/app/(authPages)/register/page.jsx
@@ -0,0 +1,9 @@
+const RegisterPage = () => {
+  return (
+    <div>
+      <p className="font-bold text-3xl">this is register page</p>
+    </div>
+  );
+};
+
+export default RegisterPage;
