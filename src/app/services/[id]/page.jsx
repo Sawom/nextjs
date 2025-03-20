85f2acc4937984016d5b55d54014b475d3adcc88
Sawom <asawom250@gmail.com>
2025-03-21 00:23:19 +0600

    services added

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,8 +1,215 @@
 // import { useRouter } from "next/router";
 export default function ServiceDetailsPage({ params }) {
-  return (
-    <div>
-      <h2>service details page</h2>
-    </div>
-  );
+  [
+    {
+      _id: "635a0c0b64a6d231228942ae",
+      service_id: "04",
+      title: "Engine Oil Change",
+      img: "https://i.ibb.co/T2cpBd5/888.jpg",
+      price: "20.00",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+      facility: [
+        {
+          name: "Instant Car Services",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "24/7 Quality Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "Easy Customer Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "Quality Cost Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+      ],
+    },
+    {
+      _id: "635a0c0b64a6d231228942af",
+      service_id: "05",
+      title: "Battery Charge",
+      img: "https://i.ibb.co/ydCbDN3/5555.jpg",
+      price: "20.00",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+      facility: [
+        {
+          name: "Instant Car Services",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "24/7 Quality Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "Easy Customer Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "Quality Cost Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+      ],
+    },
+    {
+      _id: "635b591a1dafe382a9da8c96",
+      service_id: "01",
+      title: "Full car Repair",
+      img: "https://i.ibb.co/R6Z2nFM/55.jpg",
+      price: "200.00",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+      facility: [
+        {
+          name: "Instant Car Services",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "24/7 Quality Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "Easy Customer Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "Quality Cost Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+      ],
+    },
+    {
+      _id: "635b5afc1dafe382a9da8c98",
+      service_id: "02",
+      title: "Engine Repair",
+      img: "https://i.ibb.co/5MvmD2g/88.jpg",
+      price: "150.00",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+      facility: [
+        {
+          name: "Instant Car Services",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "24/7 Quality Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "Easy Customer Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "Quality Cost Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+      ],
+    },
+    {
+      _id: "635b5b691dafe382a9da8c99",
+      service_id: "03",
+      title: "Automatic Services",
+      img: "https://i.ibb.co/wh7t3N3/555.jpg",
+      price: "30.00",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+      facility: [
+        {
+          name: "Instant Car Services",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "24/7 Quality Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "Easy Customer Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "Quality Cost Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+      ],
+    },
+    {
+      _id: "635b5ba51dafe382a9da8c9a",
+      service_id: "06",
+      title: "Electrical System",
+      img: "https://i.ibb.co/KzCG8qr/8888.jpg",
+      price: "20.00",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+      facility: [
+        {
+          name: "Instant Car Services",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "24/7 Quality Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "Easy Customer Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+        {
+          name: "Quality Cost Service",
+          details:
+            "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum.",
+        },
+      ],
+    },
+  ];
+
+  const id = params?.id;
+  const singleData = data.find((d) => d._id == id);
+
+  if (singleData) {
+    return (
+      <div>
+        <h2>service details page</h2>
+
+        <div className="flex items-center">
+          <div className="max-w-[400px] space-y-4">
+            <p>id: {id} </p>
+            <p>title: {singleData.title}</p>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <p>not found</p>
+      </>
+    );
+  }
 }
